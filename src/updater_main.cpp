27f15ff6bc2f793acfc8e74aedb277f72321bb39
Jennifer He <jennifer.he.chinghuen@gmail.com>
2026-04-22 21:35:34 -0400

    comments

--- src/updater_main.cpp
+++ src/updater_main.cpp
@@ -1,4 +1,3 @@
-// Main program for the updater client
 
 #include <iostream>
 #include <string>
@@ -15,11 +14,13 @@
 
 namespace {
 
+// tie the lifetime of a file descriptor to an object
 class FdGuard {
 private:
   int m_fd;
 
 public:
+  // explicit so that this constructor cannot be used for implicit conversions
   explicit FdGuard(int fd)
     : m_fd(fd) {
   }
@@ -30,6 +31,8 @@ public:
   }
 };
 
+
+// helper functions for handling commands
 void print_result_response(const Message &response) {
   if (response.get_type() == MessageType::OK) {
     std::cout << "Success: " << response.get_str() << "\n";
@@ -48,13 +51,17 @@ Message send_request_receive_response(int fd, const Message &request) {
   return response;
 }
 
+
+// Handle commands
 void handle_order_new(int fd) {
   std::string line;
   std::getline(std::cin, line);
   int num_items = ClientUtil::parse_positive_int(line, "number of items");
 
+  // create a new order object with a temporary order id of 1 and status of NEW
   auto order = std::make_shared<Order>(1, OrderStatus::NEW);
 
+  // for each item, read item id, description, and quantity from stdin and add to order
   for (int i = 0; i < num_items; ++i) {
     std::string item_id_s;
     std::string item_desc;
@@ -67,9 +74,11 @@ void handle_order_new(int fd) {
     int item_id = ClientUtil::parse_positive_int(item_id_s, "item id");
     int item_qty = ClientUtil::parse_positive_int(item_qty_s, "item quantity");
 
+    // create a new item: 
     order->add_item(std::make_shared<Item>(1, item_id, ItemStatus::NEW, item_desc, item_qty));
   }
 
+  // send the request and print the response
   Message request(MessageType::ORDER_NEW, order);
   Message response = send_request_receive_response(fd, request);
   print_result_response(response);
@@ -80,10 +89,12 @@ void handle_item_update(int fd) {
   std::string item_id_s;
   std::string item_status_s;
 
+  // read order id, item id, and item status from stdin
   std::getline(std::cin, order_id_s);
   std::getline(std::cin, item_id_s);
   std::getline(std::cin, item_status_s);
 
+  // parse order id and item id as positive integers
   int order_id = ClientUtil::parse_positive_int(order_id_s, "order id");
   int item_id = ClientUtil::parse_positive_int(item_id_s, "item id");
 
@@ -91,6 +102,7 @@ void handle_item_update(int fd) {
   if (item_status == ItemStatus::INVALID)
     throw ProtocolError("invalid item status");
 
+  // send the request and print the response
   Message request(MessageType::ITEM_UPDATE, order_id, item_id, item_status);
   Message response = send_request_receive_response(fd, request);
   print_result_response(response);
@@ -103,11 +115,13 @@ void handle_order_update(int fd) {
   std::getline(std::cin, order_id_s);
   std::getline(std::cin, order_status_s);
 
+  // parse order id as positive integer
   int order_id = ClientUtil::parse_positive_int(order_id_s, "order id");
   OrderStatus order_status = Wire::str_to_order_status(order_status_s);
   if (order_status == OrderStatus::INVALID)
     throw ProtocolError("invalid order status");
 
+  // send the request and print the response
   Message request(MessageType::ORDER_UPDATE, order_id, order_status);
   Message response = send_request_receive_response(fd, request);
   print_result_response(response);
@@ -115,20 +129,28 @@ void handle_order_update(int fd) {
 
 }
 
+// main program for updater client
 int main(int argc, char **argv) {
+
+  // Check command line arguments: expect hostname and port
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " <hostname> <port>\n";
     return 1;
   }
 
   try {
-    int fd = ClientUtil::connect_to_server(argv[1], argv[2]);
-    FdGuard guard(fd);
 
-    std::string credentials = ClientUtil::prompt_for_credentials();
-    Message login_req(MessageType::LOGIN, ClientMode::UPDATER, credentials);
-    Message login_response = send_request_receive_response(fd, login_req);
+    int filedescriptor = ClientUtil::connect_to_server(argv[1], argv[2]);
+    FdGuard guard(filedescriptor);
 
+    // Prompt for credentials and log in
+    std::string user_typed_credentials = ClientUtil::prompt_for_credentials();
+
+    // create a login message and send it 
+    Message login_req(MessageType::LOGIN, ClientMode::UPDATER, user_typed_credentials);
+    Message login_response = send_request_receive_response(filedescriptor, login_req);
+
+    // check the response
     if (login_response.get_type() == MessageType::ERROR) {
       std::cerr << "Error: " << login_response.get_str() << "\n";
       return 1;
@@ -136,36 +158,42 @@ int main(int argc, char **argv) {
     if (login_response.get_type() != MessageType::OK)
       throw ProtocolError("Unexpected response type");
 
+    // If we get here, login successful
+    // start the loop waiting for comamnds
     while (true) {
+      // print prompt
       std::cout << "> " << std::flush;
 
+      // if user types EOF (e.g. ctrl-d), exit the loop and close the connection
       std::string command;
-      if (!std::getline(std::cin, command))
+      if (!std::getline(std::cin, command)) // get line returns false if EOF
         break;
 
+      // or if user types "quit", send quit message to server and exit
       if (command == "quit") {
         Message quit_req(MessageType::QUIT, "quit");
-        Message response = send_request_receive_response(fd, quit_req);
+        Message response = send_request_receive_response(filedescriptor, quit_req);
         if (response.get_type() != MessageType::OK)
           throw ProtocolError("Unexpected response type");
         return 0;
       }
 
+      // other commands: order_new, item_update, order_update
       if (command == "order_new") {
-        handle_order_new(fd);
+        handle_order_new(filedescriptor);
       } else if (command == "item_update") {
-        handle_item_update(fd);
+        handle_item_update(filedescriptor);
       } else if (command == "order_update") {
-        handle_order_update(fd);
+        handle_order_update(filedescriptor);
       }
     }
 
     return 0;
-  } catch (Exception &ex) {
-    std::cerr << "Error: " << ex.what() << "\n";
+  } catch (Exception &e) {
+    std::cerr << "Error: " << e.what() << "\n";
     return 1;
   } catch (std::exception &ex) {
-    std::cerr << "Error: " << ex.what() << "\n";
+    std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
 }
